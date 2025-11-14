c4202ff5d463e734d043740d1fe1ba32e6061b18
Temurbek Reyimberdiyev <temurbekreyimberdiyev@gmail.com>
2025-11-14 14:54:27 +0500

    fixed

--- api/views.py
+++ api/views.py
@@ -1,34 +1,39 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializers import *
 
 class SkillViewSet(viewsets.ModelViewSet):
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
+    permission_classes = [IsAuthenticated]  # 🔐 himoya
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
 
 class ExperienceViewSet(viewsets.ModelViewSet):
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
+    permission_classes = [IsAuthenticated]
 
 class HeroSectionViewSet(viewsets.ModelViewSet):
     queryset = HeroSection.objects.all()
     serializer_class = HeroSectionSerializer
+    permission_classes = [IsAuthenticated]
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+    permission_classes = [IsAuthenticated]
 
 class SocialLinkViewSet(viewsets.ModelViewSet):
     queryset = SocialLink.objects.all()
     serializer_class = SocialLinkSerializer
+    permission_classes = [IsAuthenticated]
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
